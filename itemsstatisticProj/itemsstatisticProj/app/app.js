8346e6a08c2c696339006c0a8179d7ecbc948a2b
Andrii Kolomys <pinoplastiks@gmail.com>
2017-11-28 17:20:59 +0200

    Add pagination template

--- itemsstatisticProj/itemsstatisticProj/app/app.js
+++ itemsstatisticProj/itemsstatisticProj/app/app.js
@@ -1,4 +1,4 @@
-﻿angular.module('app', ['components'])
+﻿angular.module('app', ['components', 'controllers'])
 
     .controller('BeerCounter', function ($scope, $locale) {
         $scope.beers = [0, 1, 2, 3, 4, 5, 6];
@@ -17,3 +17,38 @@
             };
         }
     });
+    //.filter("startFrom", function () {
+    //    return function (input, start) {
+    //        start = +start;
+    //        return input.slice(start);
+    //    };
+    //})
+    //.controller("paginationController", function ($scope) {
+    //    $scope.currentPage = 0;
+    //    $scope.itemsPerPage = 5;
+    //    $scope.items = [];
+    //    for (var i = 0; i < 25; i++) {
+    //        $scope.items.push("Product " + i);
+    //    }
+    //    $scope.firstPage = function () {
+    //        return $scope.currentPage == 0;
+    //    };
+    //    $scope.lastPage = function () {
+    //        var lastPageNum = Math.ceil(
+    //            $scope.items.length / $scope.itemsPerPage - 1
+    //        );
+    //        return $scope.currentPage == lastPageNum;
+    //    };
+    //    $scope.numberOfPages = function () {
+    //        return Math.ceil($scope.items.length / $scope.itemsPerPage);
+    //    };
+    //    $scope.startingItem = function () {
+    //        return $scope.currentPage * $scope.itemsPerPage;
+    //    };
+    //    $scope.pageBack = function () {
+    //        $scope.currentPage = $scope.currentPage - 1;
+    //    };
+    //    $scope.pageForward = function () {
+    //        $scope.currentPage = $scope.currentPage + 1;
+    //    };
+    //});
---
+++ itemsstatisticProj/itemsstatisticProj/app/controllers/paginationController.js
@@ -0,0 +1,36 @@
+﻿angular.module("controllers", []).filter("startFrom", function () {
+    return function (input, start) {
+        start = +start;
+        return input.slice(start);
+    };
+})
+.controller("paginationController", function ($scope) {
+    $scope.currentPage = 0;
+    $scope.itemsPerPage = 5;
+    $scope.items = [];
+    for (var i = 0; i < 25; i++) {
+        $scope.items.push("Product " + i);
+    }
+    $scope.firstPage = function () {
+        return $scope.currentPage == 0;
+    };
+    $scope.lastPage = function () {
+        var lastPageNum = Math.ceil(
+            $scope.items.length / $scope.itemsPerPage - 1
+        );
+        return $scope.currentPage == lastPageNum;
+    };
+    $scope.numberOfPages = function () {
+        return Math.ceil($scope.items.length / $scope.itemsPerPage);
+    };
+    $scope.startingItem = function () {
+        return $scope.currentPage * $scope.itemsPerPage;
+    };
+    $scope.pageBack = function () {
+        $scope.currentPage = $scope.currentPage - 1;
+    };
+    $scope.pageForward = function () {
+        $scope.currentPage = $scope.currentPage + 1;
+    };
+});
+
